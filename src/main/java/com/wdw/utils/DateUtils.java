8c4c27b7d3fea83947708de7344dfe455f5a16f8
wangdaiwu <1443110344@qq.com>
2022-04-15 17:55:06 +0800

    Update DateUtils.java

--- src/main/java/com/wdw/utils/DateUtils.java
+++ src/main/java/com/wdw/utils/DateUtils.java
@@ -7,13 +7,11 @@ import java.util.TimeZone;
 public class DateUtils {
     public static String logDate() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-//        dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
         return dateFormat.format(new Date());
     }
 
     public static String fileDate() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss");
-//        dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Shanghai"));
         return dateFormat.format(new Date());
     }
 }
